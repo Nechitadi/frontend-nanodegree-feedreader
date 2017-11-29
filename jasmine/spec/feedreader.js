bbd108f23e117b3c594ba80ccc92458e02645bed
Nechitadi <nechitadi11@gmail.com>
2017-11-29 15:02:53 +0200

    Write a new test suite named "Initial Entries"

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -73,22 +73,35 @@ $(function() {
             let menuIconLink = document.getElementsByClassName('menu-icon-link')[0];
             let body = document.getElementsByTagName('body')[0];
             menuIconLink.click();
-            expect(body.className).toBe('');
+            expect(body.className).not.toBe('menu-hidden');
             menuIconLink.click();
             expect(body.className).toBe('menu-hidden');
         });
     });
-    /* TODO: Write a new test suite named "Initial Entries" */
 
+    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        it('should be at least a single .entry element within the .feed container after loadFeed completes it\'s work', function(done) {
+            // let feedContainer = document.getElementsByClassName('feed');
+            // let entry = document.querySelector('.entry');
+            expect($('.feed').children().length).toBeGreaterThan(0);
+            //expect($('.feed').firstChild.hasClass('entry')).toBeTruthy();
+            done();
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
